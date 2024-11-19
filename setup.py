5236a0021cd8abbd8f545bc599efd37e9ddbaa75
Ankit141731 <ac7265036@gmail.com>
2024-11-19 23:07:48 +0530

    installed requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup , find_packages
+
+setup(
+    name = "us_Visa",
+    version = "0.0.0",
+    author = "ankit",
+    author_email = "ac7265036@gmail.com",
+    packages = find_packages()
+
+)
